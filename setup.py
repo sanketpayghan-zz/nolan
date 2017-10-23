850253db511d82643f56a26b98a0fd2ea45f19b5
Sanket Payghan <sanket.p@happay.in>
2017-10-23 13:24:15 +0530

    added current directory path in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup, Extension
+import os
 
+path_str = os.getcwd()
 module1 = Extension('nolan',
 					libraries = ['call'],
-					library_dirs = ['/home/Happay_v2/Happay_v2/nolan'],
+					library_dirs = [path_str],
 					sources = ['nolan.c'])
 setup (name = 'PackageName',version = '1.0',description = 'This is module can be used to make paralle http requests.',ext_modules = [module1])
